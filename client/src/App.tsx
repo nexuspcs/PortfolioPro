df6d4efe668cc98d6c57c28720ab023da2e54e5a
npcs <69493073+nexuspcs@users.noreply.github.com>
2024-03-30 14:20:20 +1100

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,20 @@
 import { CssBaseline, ThemeProvider } from "@mui/material/";
 import { createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
+import { BrowserRouter } from "react-router-dom";
 import { themeSettings } from "./theme";
+
 
 function App() { 
   const theme = useMemo(() => createTheme(themeSettings), [])
   return (
     <div className="App">
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        hello world
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          hello world
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
