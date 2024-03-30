88f6dc3cb3aed26a51a95fbee2ad4c3b55372e35
npcs <69493073+nexuspcs@users.noreply.github.com>
2024-03-30 14:22:18 +1100

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -13,7 +13,7 @@ function App() {
       <BrowserRouter>
         <ThemeProvider theme={theme}>
           <CssBaseline />
-          <Box width="100%" height="100%">
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
 
           </Box>
         </ThemeProvider>
