024fee78d1f0f5f11d6abdc9c2404113276e56fc
npcs <69493073+nexuspcs@users.noreply.github.com>
2024-03-30 14:36:37 +1100

    Making the navbar using a flexbox+

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import { createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { themeSettings } from "./theme";
+import Navbar from "@/scenes/navbar";
 
 
 function App() { 
@@ -14,6 +15,7 @@ function App() {
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Navbar />
             <Routes>
               <Route path="/" element={<div>Dashboard Page</div>} />
               <Route path="/predictions" element={<div>Predictions Page</div>} />
