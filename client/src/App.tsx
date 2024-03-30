b8a64132c2df740d77c69bb9bc236a83aba62cc1
npcs <69493073+nexuspcs@users.noreply.github.com>
2024-03-30 14:25:06 +1100

    Creating routes which are essentilly an easy way to use a premade navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import { Box } from "@mui/material";
 import { CssBaseline, ThemeProvider } from "@mui/material/";
 import { createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { themeSettings } from "./theme";
 
 
@@ -14,7 +14,10 @@ function App() {
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
-
+            <Routes>
+              <Route path="/" element={<div>Dashboard Page</div>} />
+              <Route path="/predictions" element={<div>Predictions Page</div>} />
+            </Routes>
           </Box>
         </ThemeProvider>
       </BrowserRouter>
