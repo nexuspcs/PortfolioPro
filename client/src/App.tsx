1372cbaa167f999c1b0e5b1d2d87f09a01ced87b
npcs <69493073+nexuspcs@users.noreply.github.com>
2024-03-30 14:14:27 +1100

    Added a themeprovider class

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,14 @@
+import { CssBaseline, ThemeProvider } from "@mui/material/";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { themeSettings } from "./theme";
+
 function App() { 
+  const theme = useMemo(() => createTheme(themeSettings), [])
   return <div className="App"></div>;
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+    </ThemeProvider>
 }
 
 export default App;
