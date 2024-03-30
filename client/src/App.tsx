b150f3ef01c78121c0a4840c1815673b6bca5d59
npcs <69493073+nexuspcs@users.noreply.github.com>
2024-03-30 14:52:01 +1100

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import { Box } from "@mui/material";
 import { CssBaseline, ThemeProvider } from "@mui/material/";
 import { createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { themeSettings } from "./theme";
 import Navbar from "@/scenes/navbar";
 
@@ -15,11 +15,15 @@ function App() {
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+<<<<<<< HEAD
             <Navbar />
             <Routes>
               <Route path="/" element={<div>Dashboard Page</div>} />
               <Route path="/predictions" element={<div>Predictions Page</div>} />
             </Routes>
+=======
+
+>>>>>>> parent of b8a6413 (Creating routes which are essentilly an easy way to use a premade navbar)
           </Box>
         </ThemeProvider>
       </BrowserRouter>
