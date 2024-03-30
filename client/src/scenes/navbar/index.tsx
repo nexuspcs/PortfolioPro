81de78ce0f42f978ce88b8af9f4d1362caeb2e72
npcs <69493073+nexuspcs@users.noreply.github.com>
2024-03-30 14:50:26 +1100

    Update index.tsx

--- client/src/scenes/navbar/index.tsx
+++ client/src/scenes/navbar/index.tsx
@@ -1,19 +1,21 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { Box, Typography, useTheme } from '@mui/material';
-import FlexBetween from '@/components/FlexBetween';
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import PixIcon from "@mui/icons-material/Pix";
+import { Box, Typography, useTheme } from "@mui/material";
+import FlexBetween from "@/components/FlexBetween";
 
-type Props = {}
+type Props = {};
 
 const Navbar = (props: Props) => {
     const { palette } = useTheme();
     const [selected, setSelected] = useState("Dashboard");
     return (
-        <FlexBetween 
-            mb="0.25rem" 
-            p="0.5rem 0 rem" 
-            color={palette.grey[300]}
-        ></FlexBetween>
+        <FlexBetween mb="0.25rem" p="0.5rem 0 rem" color={palette.grey[300]}>
+            {/* left side */}
+            <FlexBetween gap="0.75rem">
+                <PixIcon />
+            </FlexBetween>
+        </FlexBetween>
     );
 };
 
