1984caac0aed95421e59fb716225320d7b1f79c9
npcs <69493073+nexuspcs@users.noreply.github.com>
2024-03-30 14:40:09 +1100

    Updated to use the flexbox

--- client/src/scenes/navbar/index.tsx
+++ client/src/scenes/navbar/index.tsx
@@ -1,17 +1,18 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Box, Typography, useTheme } from '@mui/material';
+import FlexBetween from '@/components/FlexBetween';
 
 type Props = {}
 
 const Navbar = (props: Props) => {
     const { palette } = useTheme();
     return (
-        <Box 
+        <FlexBetween 
             display="flex" 
             justifyContent="space-between" 
             alignItems="center"
-        ></Box>
+        ></FlexBetween>
     );
 };
 
