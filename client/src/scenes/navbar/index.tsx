6082a07f634597615a127325a2ad42722bf2ba0d
npcs <69493073+nexuspcs@users.noreply.github.com>
2024-04-18 18:12:50 +1000

    add text portfoliopro

--- client/src/scenes/navbar/index.tsx
+++ client/src/scenes/navbar/index.tsx
@@ -13,7 +13,8 @@ const Navbar = (props: Props) => {
     <FlexBetween mb="0.25rem" p="0.5rem 0rem" color={palette.grey[300]}>
       {/* LEFT SIDE */}
       <FlexBetween gap="0.75rem">
-        <PixIcon />
+        <PixIcon sx={{ fontSize: "28px" }}/>
+        <Typography variant="h4" fontSize="16px">Portfolio Pro</Typography>
       </FlexBetween>
     </FlexBetween>
   );
