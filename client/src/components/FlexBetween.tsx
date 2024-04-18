fbfd2a60192dafb9a6df20aafddf0087021852ab
npcs <69493073+nexuspcs@users.noreply.github.com>
2024-04-18 16:00:43 +1000

    universnal file for navbar

--- client/src/components/FlexBetween.tsx
+++ client/src/components/FlexBetween.tsx
@@ -1,5 +1,5 @@
 import { Box } from "@mui/material";
-import styled from "@mui/system";
+import { styled } from "@mui/system"; // Add the missing import statement
 
 const FlexBetween = styled(Box)({
     display: "flex",
--- client/src/scenes/navbar/index.tsx
+++ client/src/scenes/navbar/index.tsx
@@ -1,19 +1,20 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { Box, Typography, useTheme } from '@mui/material';
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { Box, Typography, useTheme } from "@mui/material";
+import FlexBetween from "@/components/FlexBetween";
 
 type Props = {};
 
 const Navbar = (props: Props) => {
-    const { palette } = useTheme();
-    const [selected, setSelected] = useState("Dashboard");
-    return (
-        <Box 
-            display="flex" 
-            justifyContent="space-between" 
-            alignItems="center"
-        ></Box>
-    );
+  const { palette } = useTheme();
+  const [selected, setSelected] = useState("Dashboard"); //Highlight to the user what page they are on .
+  return (
+    <FlexBetween
+      mb="0.25rem"
+      p="0.5rem 0rem"
+      color={palette.grey[300]}
+    ></FlexBetween>
+  );
 };
 
-export default Navbar
+export default Navbar;
