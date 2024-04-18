8b22be62d10f64e2c24fc3c4ad661d354c1ba3c4
npcs <69493073+nexuspcs@users.noreply.github.com>
2024-04-18 16:03:43 +1000

    Added a placeholder ICON to the dashbaord using MUI

--- client/src/scenes/navbar/index.tsx
+++ client/src/scenes/navbar/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import PixIcon from "@mui/icons-material/Pix";
 import { Box, Typography, useTheme } from "@mui/material";
 import FlexBetween from "@/components/FlexBetween";
 
@@ -9,11 +10,12 @@ const Navbar = (props: Props) => {
   const { palette } = useTheme();
   const [selected, setSelected] = useState("Dashboard"); //Highlight to the user what page they are on .
   return (
-    <FlexBetween
-      mb="0.25rem"
-      p="0.5rem 0rem"
-      color={palette.grey[300]}
-    ></FlexBetween>
+    <FlexBetween mb="0.25rem" p="0.5rem 0rem" color={palette.grey[300]}>
+      {/* LEFT SIDE */}
+      <FlexBetween gap="0.75rem">
+        <PixIcon />
+      </FlexBetween>
+    </FlexBetween>
   );
 };
 
