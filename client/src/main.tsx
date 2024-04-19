1fb89c9bddda70e2d948c7b3d508bdc4801c6f3d
npcs <69493073+nexuspcs@users.noreply.github.com>
2024-04-19 15:20:26 +1000

    adding fucntionality to grab the api

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from '@/App.tsx'
-import '@/index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "@/App.tsx";
+import "@/index.css";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { api } from "@/state/api"; // grabbing the api
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
