c114b5b6c4d7ce053e71093341ca548ac285af41
npcs <69493073+nexuspcs@users.noreply.github.com>
2024-04-19 15:32:38 +1000

    Installing all of the packages needed for the server express body-parser cors dotenv helmet morgan mongoose mongoose-currency

---
+++ server/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+
+/* Configurations (mainly used as a boiler plate to config these pakages in the long run m*/
+dotenv.config();
+const app = express();
+app.use(exprrss.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
