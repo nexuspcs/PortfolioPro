37d427a5f376f10bb7f39d9c1bf1226455a22af9
npcs <69493073+nexuspcs@users.noreply.github.com>
2024-04-19 15:15:28 +1000

    updating the boilerplate

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -2,4 +2,12 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
-})
+    reducerPath: "main", // name for the api calls doesnt matter really just a name
+    tagTypes: ["Kpis"], // used to keep info
+    endpoints: (build) => ({
+        getKpis: build.query({
+            query: () => "kpi/kpis/",
+            providesTags: ["Kpis"],
+        }),
+    }),
+});
