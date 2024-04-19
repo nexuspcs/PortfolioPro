e3ae0eb193ada5d2d5bb506ea22dfa150b30b03c
npcs <69493073+nexuspcs@users.noreply.github.com>
2024-04-19 15:05:25 +1000

    Made the rows a lot more effecitent by moving their defintiions to their own files row1.tsx row2.tsx row3.tsx etc

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -1,10 +1,9 @@
-import DashboardBox from "@/components/DashboardBox";
 import { Box, useMediaQuery } from "@mui/material";
+import Row1 from "./Row1";
+import Row2 from "./Row2";
+import Row3 from "./Row3";
 
-// Defining Props type as an empty object for Dashboard component
-type Props = {};
 
-// Defining a grid template as a string for the layout of the dashboard
 const gridTemplateLargeScreens = `
     "a b c"
     "a b c"
@@ -49,8 +48,7 @@ const gridTemplateSmallScreens = `
     "j"
 `;
 
-// Dashboard functional component definition
-const Dashboard = (props: Props) => {
+const Dashboard = () => {
   const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)");
   return (
     <Box
@@ -72,16 +70,11 @@ const Dashboard = (props: Props) => {
             }
       }
     >
-      <DashboardBox gridArea="a"></DashboardBox>
-      <DashboardBox gridArea="b"></DashboardBox>
-      <DashboardBox gridArea="c"></DashboardBox>
-      <DashboardBox gridArea="d"></DashboardBox>
-      <DashboardBox gridArea="e"></DashboardBox>
-      <DashboardBox gridArea="f"></DashboardBox>
-      <DashboardBox gridArea="g"></DashboardBox>
-      <DashboardBox gridArea="h"></DashboardBox>
-      <DashboardBox gridArea="i"></DashboardBox>
-      <DashboardBox gridArea="j"></DashboardBox>
+        <Row1/>
+        <Row2/>
+        <Row3/>
+      
+      
     </Box>
   );
 };
