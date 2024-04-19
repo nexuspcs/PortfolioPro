fce1f302694bbeb1ebecf06016ee1abeebbfd2f7
npcs <69493073+nexuspcs@users.noreply.github.com>
2024-04-19 14:43:49 +1000

    grid template screen making it full 3 coloum layout on screens > 1200px

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -1,11 +1,11 @@
 // Importing Box component and useTheme hook from Material-UI
-import { Box, useTheme } from "@mui/material";
+import { Box, useMediaQuery, useTheme } from "@mui/material";
 
 // Defining Props type as an empty object for Dashboard component
 type Props = {};
 
 // Defining a grid template as a string for the layout of the dashboard
-const gridTemplate = `
+const gridTemplateLargeScreens = `
     "a b c"
     "a b c"
     "a b c"
@@ -17,9 +17,41 @@ const gridTemplate = `
     "g h j"
     "g h j"
 `;
+const gridTemplateSmallScreens = `
+    "a"
+    "a"
+    "a"
+    "a"
+    "b"
+    "b"
+    "b"
+    "c"
+    "c"
+    "c"
+    "d"
+    "d"
+    "d"
+    "e"
+    "e"
+    "f"
+    "f"
+    "f"
+    "g"
+    "g"
+    "g"
+    "h"
+    "h"
+    "h"
+    "h"
+    "i"
+    "i"
+    "j"
+    "j"
+`;
 
 // Dashboard functional component definition
 const Dashboard = (props: Props) => {
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)");
   // Using the useTheme hook to access the theme object and destructuring to get the palette
   const { palette } = useTheme();
   // Returning a Box component styled as a grid to represent the dashboard layout
@@ -29,13 +61,19 @@ const Dashboard = (props: Props) => {
       height="100%"
       display="grid"
       gap="1.5rem"
-      sx={{
-        // Setting the grid's columns to repeat 3 times with a minimum width and flexible growth
-        gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
-        gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
-        // Applying the grid template areas defined above to layout the grid items
-        gridTemplateAreas: gridTemplate,
-      }}
+      sx={
+        isAboveMediumScreens
+          ? {
+              gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
+              gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
+              gridTemplateAreas: gridTemplateLargeScreens,
+            }
+          : {
+              gridAutoColumns: "1fr",
+              gridAutoRows: "80px",
+              gridTemplateAreas: gridTemplateSmallScreens,
+            }
+      }
     >
       <Box bgcolor="#fff" gridArea="a"></Box>
       <Box bgcolor="#fff" gridArea="b"></Box>
