0145ad5971d35f3758688c4971d58d16e2b9679c
npcs <69493073+nexuspcs@users.noreply.github.com>
2024-04-19 14:29:12 +1000

    active boxes to flex and adapt added all for all categories

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -16,7 +16,7 @@ const gridTemplate = `
     "g h i"
     "g h j"
     "g h j"
-`
+`;
 
 // Dashboard functional component definition
 const Dashboard = (props: Props) => {
@@ -24,15 +24,29 @@ const Dashboard = (props: Props) => {
   const { palette } = useTheme();
   // Returning a Box component styled as a grid to represent the dashboard layout
   return (
-    <Box width="100%" height="100%" display="grid" gap="1.5rem"
-    sx={{
+    <Box
+      width="100%"
+      height="100%"
+      display="grid"
+      gap="1.5rem"
+      sx={{
         // Setting the grid's columns to repeat 3 times with a minimum width and flexible growth
         gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
+        gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
         // Applying the grid template areas defined above to layout the grid items
         gridTemplateAreas: gridTemplate,
-    }}
+      }}
     >
-      Dashboard
+      <Box bgcolor="#fff" gridArea="a"></Box>
+      <Box bgcolor="#fff" gridArea="b"></Box>
+      <Box bgcolor="#fff" gridArea="c"></Box>
+      <Box bgcolor="#fff" gridArea="d"></Box>
+      <Box bgcolor="#fff" gridArea="e"></Box>
+      <Box bgcolor="#fff" gridArea="f"></Box>
+      <Box bgcolor="#fff" gridArea="g"></Box>
+      <Box bgcolor="#fff" gridArea="h"></Box>
+      <Box bgcolor="#fff" gridArea="i"></Box>
+      <Box bgcolor="#fff" gridArea="j"></Box>
     </Box>
   );
 };
