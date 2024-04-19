7d86b3100a5b36984a55013e857c78ac0f38b0b3
npcs <69493073+nexuspcs@users.noreply.github.com>
2024-04-19 12:31:27 +1000

    Updating the COLOR

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -1,11 +1,11 @@
+import { Box, useTheme } from '@mui/material'
 import React from 'react'
 
 type Props = {}
 
 const Dashboard = (props: Props) => {
-  return (
-    <div>Dashboard</div>
-  )
+    const { palette } = useTheme();
+    return <Box color={palette.grey[300]}>Dashboard</Box>
 }
 
 export default Dashboard
