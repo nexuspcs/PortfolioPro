a43490ca80a1ffe83ef3de2948d74a4eaf2e6ebe
npcs <69493073+nexuspcs@users.noreply.github.com>
2024-05-26 16:22:21 +1000

    Rollback

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -1,10 +1,7 @@
 import { Box, useMediaQuery } from "@mui/material";
-import React, { useEffect, useState } from 'react'; // pull data FX
 import Row1 from "./Row1";
 import Row2 from "./Row2";
 import Row3 from "./Row3";
-import getLatestFXRate from "./curr1tocurr2compar"; // pull data FX 
-
 
 
 const gridTemplateLargeScreens = `
