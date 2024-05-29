6a1dfdbee3e049638bcfa45158d9ce779b9d6fc5
npcs <69493073+nexuspcs@users.noreply.github.com>
2024-05-29 23:01:14 +1000

    foex chart debugging

--- REHAUL/finance-dashboard/src/App.js
+++ REHAUL/finance-dashboard/src/App.js
@@ -2,14 +2,13 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import './App.css';
 import StockChart from './components/StockChart';
-import ForexChart from './components/ForexChart';
+import ForexChart from './components/ForexChart.tsx';
 import TimeInCities from './components/TimeInCities';
 import PortfolioValue from './components/PortfolioValue';
 import OldAllocation from './components/OldAllocation.tsx';
 
 const App = () => {
   return (
-
     <>
       <div><button onClick={() => console.log('clicked')}>Click me</button></div>
 
@@ -18,9 +17,17 @@ const App = () => {
 
         {/* <div className="box"><StockChart symbol="AAPL" /></div>
         <div className="box"><PortfolioValue portfolio="myPortfolio" /></div>   */}
+
         <div className="box"><OldAllocation /></div>
-        <div className="box"><ForexChart fromCurrency="EUR" toCurrency="USD" /></div><div className="box"><TimeInCities /></div>
-      </div></>
+
+        <div className="box"><ForexChart
+          fromCurrency="USD"
+          toCurrency="AUD"
+          apiKey="ftlbKUoHBSCOuRGoNB3q"
+        /></div>
+        
+        <div className="box"><TimeInCities /></div></div>
+    </>
   );
 };
 
