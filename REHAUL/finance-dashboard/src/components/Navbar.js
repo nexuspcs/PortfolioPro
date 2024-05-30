de671cb6696a81d2a536cb9f37699068c9b80eba
npcs <69493073+nexuspcs@users.noreply.github.com>
2024-05-30 15:12:44 +1000

    Added help page

--- REHAUL/finance-dashboard/src/components/Navbar.js
+++ REHAUL/finance-dashboard/src/components/Navbar.js
@@ -1,19 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css'; // Import the CSS file for styling
 import logo from './logo.png'; // Import the logo image
 
 const Navbar = () => {
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
   return (
-    <nav className="navbar">
-      <div className="navbar-left">
-        <img src={logo} alt="Logo" className="navbar-logo" />
-        <span className="navbar-title">Portfolio Pro</span>
-      </div>
-      <div className="navbar-right">
-        {/* <a href="/" className="navbar-link">dashboard</a>
-        <span className="navbar-link navbar-link-inactive">predictions</span> */}
-      </div>
-    </nav>
+    <>
+      <nav className="navbar">
+        <div className="navbar-left">
+          <img src={logo} alt="Logo" className="navbar-logo" />
+          <span className="navbar-title">Portfolio Pro</span>
+        </div>
+        <div className="navbar-right">
+          <button className="help-button" onClick={openModal}>
+            <span className="help-icon">?</span> Help
+          </button>
+        </div>
+      </nav>
+      {isModalOpen && (
+        <div className="modal-overlay" onClick={closeModal}>
+          <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+            <span className="close-button" onClick={closeModal}>&times;</span>
+            <h2>Help</h2>
+            <p class="helpSectionTXTblock">This is the help section. Here you can find information and links to help you navigate the site.</p>
+            <ul>
+              <li><a href="/faq">FAQ</a></li>
+              <li><a href="/contact">Contact Support</a></li>
+              <li><a href="/guide">User Guide</a></li>
+            </ul>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
