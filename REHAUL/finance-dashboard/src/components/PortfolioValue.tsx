6abcdeac883502d729dd098c9ff3640edcef5914
npcs <69493073+nexuspcs@users.noreply.github.com>
2024-05-31 12:32:33 +1000

    hitting demo

--- REHAUL/finance-dashboard/src/components/PortfolioValue.tsx
+++ REHAUL/finance-dashboard/src/components/PortfolioValue.tsx
@@ -31,7 +31,7 @@ const PortfolioValue = () => {
     const [errorMessage, setErrorMessage] = useState("");
 
     const fetchHistoricalPrices = async (ticker: string) => {
-        const apiKey = "RbHkZj4JGTbZYbdBUuXobFOkZ6VDqCbE";
+        const apiKey = "demo";
         const response = await axios.get(
             `https://financialmodelingprep.com/api/v3/historical-price-full/${ticker}?serietype=line&timeseries=7&apikey=${apiKey}`
         );
@@ -46,7 +46,7 @@ const PortfolioValue = () => {
             return;
         }
 
-        const apiKey = "RbHkZj4JGTbZYbdBUuXobFOkZ6VDqCbE";
+        const apiKey = "demo";
         let portfolioValue = 0;
         let portfolioPrevValue = 0;
         const historicalDataPromises = stocks.map(async (stock) => {
