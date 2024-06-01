8ff75e4e01ac589691ba850dc9b125e465eeb487
npcs <69493073+nexuspcs@users.noreply.github.com>
2024-06-01 14:09:27 +1000

    cycling APIS....

--- REHAUL/finance-dashboard/src/components/PortfolioValue.tsx
+++ REHAUL/finance-dashboard/src/components/PortfolioValue.tsx
@@ -32,7 +32,7 @@ const PortfolioValue = () => {
     const [loading, setLoading] = useState(true);
 
     const fetchHistoricalPrices = async (ticker: string) => {
-        const apiKey = "VxLSGD9wJTouit9rQw06LqNLklLjOztx";
+        const apiKey = "UJzQdAiZos0R6JChLBLNhmjXIMbu7OXV";
         const response = await axios.get(
             `https://financialmodelingprep.com/api/v3/historical-price-full/${ticker}?serietype=line&timeseries=7&apikey=${apiKey}`
         );
@@ -49,7 +49,7 @@ const PortfolioValue = () => {
             return;
         }
 
-        const apiKey = "VxLSGD9wJTouit9rQw06LqNLklLjOztx";
+        const apiKey = "UJzQdAiZos0R6JChLBLNhmjXIMbu7OXV";
         let portfolioValue = 0;
         let portfolioPrevValue = 0;
         const historicalDataPromises = stocks.map(async (stock) => {
