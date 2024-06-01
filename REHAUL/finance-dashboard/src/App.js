f027947d12df813ac0489b1c7c5c1726da863b69
npcs <69493073+nexuspcs@users.noreply.github.com>
2024-06-01 14:46:29 +1000

    Reorder on Grid layout

--- REHAUL/finance-dashboard/src/App.js
+++ REHAUL/finance-dashboard/src/App.js
@@ -14,15 +14,17 @@ const App = () => {
       <Navbar />
       <div className="dashboard">
 
-        <div className="box"><PortfolioValue /></div>         
+        <div className="box"><PortfolioValue /></div>
 
         <div className="box"><OldAllocation /></div>
 
         <div className="box"><ForexData fromCurrency="USD" toCurrency="AUD" apiKey="D9O_X8drU_i8IZvvSfZi" /></div>
-        
-        <div className="box"><TimeInCities /></div></div>
+
+        <div className="box"><TimeInCities /></div>
 
         <div className="box"><StockChart symbol="AAPL" /></div>
+
+      </div>
 
     </>
   );
