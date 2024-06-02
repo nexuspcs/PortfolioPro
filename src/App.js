322ebe0859c35cffdae9845860715597a8ad6bba
npcs <69493073+nexuspcs@users.noreply.github.com>
2024-06-02 14:17:13 +1000

    Favicon

--- src/App.js
+++ src/App.js
@@ -12,6 +12,13 @@ const App = () => {
   return (
     <>
 
+      <head>
+        <link rel="shortcut icon" href="/images/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon.ico" />
+        <link rel="icon" sizes="32x32" href="/images/favicon.ico" />
+        <link rel="icon" sizes="16x16" href="/images/favicon.ico" />
+      </head>
+
       <Navbar />
       <div className="dashboard">
 
