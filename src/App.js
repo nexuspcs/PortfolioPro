83ae3c58dcb148cfc7e39c28966b11e28ef84ad6
npcs <69493073+nexuspcs@users.noreply.github.com>
2024-06-02 14:19:35 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,10 +13,10 @@ const App = () => {
     <>
 
       <head>
-        <link rel="shortcut icon" href="/images/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon.ico" />
-        <link rel="icon" sizes="32x32" href="/images/favicon.ico" />
-        <link rel="icon" sizes="16x16" href="/images/favicon.ico" />
+        <link rel="shortcut icon" href="/image/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/image/favicon.ico" />
+        <link rel="icon" sizes="32x32" href="/image/favicon.ico" />
+        <link rel="icon" sizes="16x16" href="/image/favicon.ico" />
       </head>
 
       <Navbar />
